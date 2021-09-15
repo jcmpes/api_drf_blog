2e3d53fbd31c37988d17aaa9e385b9302993f856
JC <jcmpes@gmail.com>
2021-09-15 08:40:09 +0200

    feat: sort posts descendingly by date

--- posts/views.py
+++ posts/views.py
@@ -19,7 +19,10 @@ class PostViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.Gener
 
     def get_queryset(self):
       """ Restrict list to only user posts. """
-      queryset = Post.objects.filter(user=self.request.user)
+      #queryset = Post.objects.filter(user=self.request.user)
+      
+      """ Return all posts sorted descendingly by date"""
+      queryset = Post.objects.all().order_by('-date')
       return queryset
 
     def get_permissions(self):
